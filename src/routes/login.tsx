93091055107e4b1bf89bafc67b31390725f27626
kuroko29 <tviben70@gmail.com>
2025-11-21 15:11:14 +0700

    Update login.tsx

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="h-[95vh] flex items-center justify-center">
       <div className="flex flex-col gap-8 w-[900px] max-w-[80vh]">
-        
+
         {/* Input Field */}
       <div className="relative w-full">
         <input
@@ -38,31 +38,36 @@ function App() {
             peer-not-placeholder-shown:-top-9
             peer-not-placeholder-shown:text-2xl
             peer-not-placeholder-shown:text-white
-          "
-        >
+          ">
           เลขบัตรประชาชน/รหัสนักศึกษา/ชื่อ-สกุล
         </label>
       </div>
 
 
         {/* Next Button */}
-        <button className="w-full bg-white/70 rounded-full py-5 text-2xl font-medium text-[#5F320F] hover:bg-orange-500 transition-transform transform hover:scale-105 active:scale-95">
+        <Link
+          to="/menu"
+         className="w-full bg-white/70 rounded-full py-5 text-2xl font-medium text-[#5F320F] hover:bg-orange-500 transition-transform transform hover:scale-105 active:scale-95">
           Next
-        </button>
+        </Link>
 
         {/* Bottom Buttons */}
-        <div className="w-full flex items-center justify-center gap-5">
+        <div className="w-full flex items-center justify-center gap-4">
           <Link 
             to="/register"
             className="flex-1 bg-white/70 rounded-full py-4 text-2xl text-[#5F320F] 
-            flex items-center justify-center
-                      hover:bg-orange-500 transition-transform hover:scale-105 active:scale-95">
-              ศิษย์เก่า/ไม่พบข้อมูล
+                      flex items-center justify-center
+                      hover:bg-orange-500 transition-transform hover:scale-105 active:scale-95"
+          >
+            ศิษย์เก่า/ไม่พบข้อมูล
           </Link>
-          <button className="flex-1 bg-purple-700 rounded-full py-4 text-white text-2xl hover:bg-purple-600 transition-transform hover:scale-105 active:scale-95">
+
+          <button className="flex-1 bg-purple-700 rounded-full py-4 text-white text-2xl
+                            hover:bg-purple-600 transition-transform hover:scale-105 active:scale-95">
             เข้าสู่ระบบด้วย ThaID
           </button>
         </div>
+
 
       </div>
     </div>
